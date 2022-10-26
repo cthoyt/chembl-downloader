027cea87c8212403411a2aca573c041ca7ed11ff
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-10-26 09:59:38 +0200

    Update downloader_checker.py

--- src/chembl_downloader/downloader_checker.py
+++ src/chembl_downloader/downloader_checker.py
@@ -2,32 +2,13 @@
 
 """Codebase for logging and checking the working of chembl_downloader."""
 
-from typing import List, Tuple
+from typing import Tuple
 from tqdm import tqdm
 import chembl_downloader
-import bioversions
-import ftplib
 
 from tabulate import tabulate
 from chembl_downloader.queries import COUNT_QUERY
-
-
-def get_version() -> List[str]:
-    """Get all version of ChEMBL."""
-
-    try:
-        latest_chembl_version = bioversions.get_version("chembl")
-    except ftplib.error_temp:  # error due to too many connected users
-        latest_chembl_version = 31
-
-    version_list = [
-        str(i)
-        for i in range(1, int(latest_chembl_version) + 1)
-    ]
-
-    # Side version in ChEMBL
-    version_list.extend(['22.1', '24.1'])
-    return sorted(version_list, reverse=True)
+from chembl_downloader import versions
 
 
 def check_downloader(version_name: str) -> Tuple[str, str, str]:
@@ -41,7 +22,7 @@ def check_downloader(version_name: str) -> Tuple[str, str, str]:
 
 
 def main():
-    chembl_versions = get_version()
+    chembl_versions = versions()
 
     headers = ['ChEMBL Version', 'Downloader working', 'Error']
     table = [
