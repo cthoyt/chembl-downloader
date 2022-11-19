09d16bc8325da9c4f2bfd4a42557292f182d0d68
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-19 17:43:17 +0100

    Add document and assay contrib functions

--- src/chembl_downloader/contrib.py
+++ src/chembl_downloader/contrib.py
@@ -9,7 +9,11 @@ import click
 import pystow
 
 from chembl_downloader.api import latest, query
-from chembl_downloader.queries import get_target_sql
+from chembl_downloader.queries import (
+    get_assay_sql,
+    get_document_molecule_sql,
+    get_target_sql,
+)
 
 if TYPE_CHECKING:
     import pandas
@@ -17,6 +21,10 @@ if TYPE_CHECKING:
 __all__ = [
     "get_target_smi_df",
     "write_target_smi_file",
+    "get_assay_smi_df",
+    "write_assay_smi_file",
+    "get_document_smi_df",
+    "write_document_smi_file",
 ]
 
 
@@ -63,10 +71,74 @@ def write_target_smi_file(
     df.to_csv(path, sep=sep, index=False, header=False)
 
 
+def get_assay_smi_df(
+    assay_chembl_id: str,
+    *,
+    version: Optional[str] = None,
+    refresh: bool = False,
+    **kwargs,
+) -> "pandas.DataFrame":
+    """Get a SMI."""
+    import pandas as pd
+
+    if version is None:
+        version = latest()
+
+    path = pystow.join("chembl", version, "assays", name=f"{assay_chembl_id}.smi")
+    if path.is_file() and not refresh:
+        df = pd.read_csv(path)
+    else:
+        sql = get_assay_sql(assay_chembl_id)
+        df = query(sql=sql, version=version, **kwargs)
+        df.to_csv(path, index=False)
+
+    return df
+
+
+def write_assay_smi_file(
+    assay_chembl_id: str, path: Path, *, version: Optional[str] = None, sep: str = ",", **kwargs
+) -> None:
+    """Write SMI file for the given assay."""
+    df = get_assay_smi_df(assay_chembl_id=assay_chembl_id, version=version, **kwargs)
+    df.to_csv(path, sep=sep, index=False, header=False)
+
+
+def get_document_smi_df(
+    document_chembl_id: str,
+    *,
+    version: Optional[str] = None,
+    refresh: bool = False,
+    **kwargs,
+) -> "pandas.DataFrame":
+    """Get a SMI."""
+    import pandas as pd
+
+    if version is None:
+        version = latest()
+
+    path = pystow.join("chembl", version, "documents", name=f"{document_chembl_id}.smi")
+    if path.is_file() and not refresh:
+        df = pd.read_csv(path)
+    else:
+        sql = get_document_molecule_sql(document_chembl_id)
+        df = query(sql=sql, version=version, **kwargs)
+        df.to_csv(path, index=False)
+
+    return df
+
+
+def write_document_smi_file(
+    document_chembl_id: str, path: Path, *, version: Optional[str] = None, sep: str = ",", **kwargs
+) -> None:
+    """Write SMI file for the given document."""
+    df = get_document_smi_df(document_chembl_id=document_chembl_id, version=version, **kwargs)
+    df.to_csv(path, sep=sep, index=False, header=False)
+
+
 @click.command()
 def _main():
-    path = Path(__file__).parent.resolve().joinpath("CHEMBL4550.smi")
-    write_target_smi_file("CHEMBL4550", path=path, version="31", aggregate=None)
+    path = Path(__file__).parent.resolve().joinpath("CHEMBL3098111.smi")
+    write_document_smi_file("CHEMBL3098111", path=path, version="31", refresh=True)
     click.echo(f"output to {path}")
 
 
