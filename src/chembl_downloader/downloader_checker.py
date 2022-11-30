2926f623066c41fdb3a4042818ef15d5a808fc4b
YojanaGadiya <yojanagadiya@gmail.com>
2022-11-30 11:15:26 +0100

    add docstring

--- src/chembl_downloader/downloader_checker.py
+++ src/chembl_downloader/downloader_checker.py
@@ -12,19 +12,21 @@ from chembl_downloader import versions
 
 
 def check_downloader(version_name: str) -> Tuple[str, str, str]:
+    """Method to test the donwloader for specific ChEMBL version."""
     try:
-        path = chembl_downloader.download_extract_sqlite(version=version_name)
-    except Exception as e:
-        return version_name, 'No', '-'
+        chembl_downloader.download_extract_sqlite(version=version_name)
+    except Exception:
+        return version_name, '-'
 
     total_compounds = chembl_downloader.query(COUNT_QUERY_SQL, version=version_name)['count'][0]
-    return version_name, 'Yes', total_compounds
+    return version_name, total_compounds
 
 
 def main():
+    """Main function to run ChEMBL downloader on all versions fo ChEMBL."""
     chembl_versions = versions()
 
-    headers = ['ChEMBL Version', 'Downloader working', 'Total compounds']
+    headers = ['ChEMBL Version', 'Total compounds']
     table = [
         check_downloader(version_name=version)
         for version in tqdm(chembl_versions)
@@ -34,7 +36,9 @@ def main():
         print('\n \n', file=file)
         print('## ChEMBL Downloader Status', file=file)
         print(
-            '> **_NOTE:_** `chembl_downloader` runs only on SQLite distributions of ChEMBL. If the run was unsuccessful, it is becuase earlier versions of ChEMBL were provided on a MongoDB distribution.',
+            '> **_NOTE:_** `chembl_downloader` runs only on SQLite distributions of ChEMBL. \
+            If the run was unsuccessful, it is becuase earlier versions of ChEMBL were provided \
+            on a MongoDB distribution.',
             file=file)
         print('\n \n', file=file)
         print(tabulate(table, headers=headers, tablefmt='github'), file=file)
