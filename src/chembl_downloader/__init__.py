fc1f5d040e430fb6e2d261a6397d727e18eecf69
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-20 15:56:43 +0100

    Update __init__.py

--- src/chembl_downloader/__init__.py
+++ src/chembl_downloader/__init__.py
@@ -1,7 +1,7 @@
 """Download, open, and query ChEMBL through SQLite."""
 
-from . import queries  # noqa:F401
-from .api import (  # noqa:F401
+from . import queries
+from .api import (
     chemfp_load_fps,
     connect,
     cursor,
@@ -24,3 +24,28 @@ from .api import (  # noqa:F401
     supplier,
     versions,
 )
+
+__all__ = [
+    "chemfp_load_fps",
+    "connect",
+    "cursor",
+    "download_chemreps",
+    "download_extract_sqlite",
+    "download_fps",
+    "download_monomer_library",
+    "download_readme",
+    "download_sdf",
+    "download_sqlite",
+    "download_uniprot_mapping",
+    "get_chemreps_df",
+    "get_date",
+    "get_monomer_library_root",
+    "get_substructure_library",
+    "get_uniprot_mapping_df",
+    "iterate_smiles",
+    "latest",
+    "queries",
+    "query",
+    "supplier",
+    "versions",
+]
